37a382b5141c463a675663353f3bc59d8fe9af1c
tomas98Fer <tomas016fernandez@gmail.com>
2021-09-02 20:39:21 -0300

    Now rollint.two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
 def main():
   print('You rolled a die')
+  roll=5
+  print(f'You rolled a {roll}')
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
+
+
 
 if __name__== "__main__":
   main()
